47749b0d049015e1c1d551dbea76c4a1c78e24bf
Thryve <sinhaaayam12@gmail.com>
2026-05-13 04:56:07 +0530

    Add proper folder structure and separate DB config

--- server/index.js
+++ server/index.js
@@ -1,26 +1,23 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
-
-const mongoose = require('mongoose')
+const connectDB = require('./config/db')
 
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log('MongoDB connection error:', err))
+connectDB()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send('Job Portal API is running')
+app.get('/', (req, res) => {
+  res.send('Job Portal API is running')
 })
 
 const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
