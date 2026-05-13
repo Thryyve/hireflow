b23a6b8ed17aa60fe46e2ce11d0c98c3b646c5af
Thryve <sinhaaayam12@gmail.com>
2026-05-14 00:29:15 +0530

    Prepare backend for deployment

--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ connectDB()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  credentials: true
+}))
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
