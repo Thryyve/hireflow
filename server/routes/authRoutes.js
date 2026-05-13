7b6f4c412eae42bfe9778494cd069b18e3017a39
Thryve <sinhaaayam12@gmail.com>
2026-05-13 06:20:57 +0530

    Add auth middleware to protect private routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,13 @@
+const { protect } = require('../middleware/authMiddleware')
 const express = require('express')
 const router = express.Router()
 const { register, login } = require('../controllers/authController')
 
 router.post('/register', register)
 router.post('/login', login)
+
+router.get('/me', protect, (req, res) => {
+  res.json({ message: 'Protected route accessed', user: req.user })
+})
 
 module.exports = router
